966fc627c60d79fcb8459d9920c096a84f7b3fcc
Franco Piergallini Guida <francopierguida@gmail.com>
2024-01-09 07:47:41 +0000

    feat(YOLOv1): update epoch processing

--- YOLOv1/train.py
+++ YOLOv1/train.py
@@ -107,20 +107,33 @@ def main():
     )
 
     for epoch in range(EPOCHS):
+        # for x, y in train_loader:
+        #    x = x.to(DEVICE)
+        #    for idx in range(8):
+        #        bboxes = cellboxes_to_boxes(model(x))
+        #        bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
+        #        plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
+
+        #    import sys
+        #    sys.exit()
+
         pred_boxes, target_boxes = get_bboxes(
-            train_loader,
-            model,
-            iou_threshold=0.5,
-            threshold=0.4
+            train_loader, model, iou_threshold=0.5, threshold=0.4
         )
 
         mean_avg_prec = mean_average_precision(
-            pred_boxes,
-            target_boxes,
-            iou_threshold=0.5,
-            box_format="midpoint"
+            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
         )
-        print(f'Train mAP: {mean_avg_prec}')
+        print(f"Train mAP: {mean_avg_prec}")
+
+        # if mean_avg_prec > 0.9:
+        #    checkpoint = {
+        #        "state_dict": model.state_dict(),
+        #        "optimizer": optimizer.state_dict(),
+        #    }
+        #    save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
+        #    import time
+        #    time.sleep(10)
 
         train_fn(train_loader, model, optimizer, loss_fn)
 
